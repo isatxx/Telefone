daf7dab42f61df33f93249318af28da85fd3bd38
Isabelle Toniolo <isabelletoniolom@gmail.com>
2023-02-20 10:15:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Faça: Uma referência que armazene uma lista com todas as teclas e imprima o valor dela na aba console.
 var listaDeTeclas = document.querySelectorAll('.tecla');
 
-//Faça: Um script que percorra a lista de teclas do AluraFone, e ao clicar em qualquer tecla do teclado, imprima o valor no campo Digite seu telefone.
+//Faça: Um script que percorra a lista de teclas e ao clicar em qualquer tecla do teclado, imprima o valor no campo(input).
 var campoTelefone = document.querySelector('.telefone');
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
@@ -11,9 +11,17 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
         campoTelefone.value = campoTelefone.value + tecla.value;
         console.log(campoTelefone.value)
     });
-
     
+//Faça: Um script que percorra a lista de teclas e ao clicar na tecla Enter/Espaço, adicione a classe ativa no elemento e também resolva o momento de retirar a classe ativa do elemento.
+    tecla.onkeydown = function(e){
+        if(e.code === 'Space' || e.code === 'Enter') {
+            tecla.classList.add('ativa')
+        }
+    }
+    tecla.onkeyup = function(e){
+        tecla.classList.remove('ativa')    
+    }
+ 
 }
 
-//Faça: Um JavaScript que percorra a lista de teclas do AluraFone, e ao clicar na tecla Enter ou Espaço, adicione a classe ativa no elemento e também resolva o momento de retirar a classe ativa do elemento.
 
